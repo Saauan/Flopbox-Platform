51a92324a3c9b033740aedf9d351ea2c69b56352
Saauan <tristan.coignion@gmail.com>
2021-03-11 23:17:47 +0100

    initial commit

---
+++ src/test/java/saauan/flopbox/FlopBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package saauan.flopbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlopBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
