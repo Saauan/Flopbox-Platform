73a83c3e96b01f4b7f931e638f57aa466d672d0e
Tristan Coignion <tristan.coignion.etu@univ-lille.fr>
2021-03-28 19:22:13 +0200

    docs: document exception package

--- src/main/java/saauan/flopbox/exceptions/ResourceAlreadyExistException.java
+++ src/main/java/saauan/flopbox/exceptions/ResourceAlreadyExistException.java
@@ -1,5 +1,8 @@
 package saauan.flopbox.exceptions;
 
+/**
+ * Thrown when a resource already exists while trying to create it
+ */
 public class ResourceAlreadyExistException extends RuntimeException {
 	public ResourceAlreadyExistException(String message) {
 		super(message);
